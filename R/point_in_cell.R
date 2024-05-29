2e800a5ebc3b1ccb84f523ca40545b69af3eae10
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2024-05-29 19:44:44 +0200

    revise point_in_cell

--- R/point_in_cell.R
+++ R/point_in_cell.R
@@ -3,28 +3,37 @@
 #' This function examines which points fall within a raster cell with data (not NA).
 #' Returns TRUE for points falling in a raster cell with data, and FALSE otherwise.
 #'
-#' @param locs An [sf::sf()] object with point coordinates.
-#' @param ras [terra::SpatRaster-class()] object
+#' @param locs An [sf::sf()] or [terra::SpatVector()] object with point coordinates,
+#' e.g. as generated from [locs2sf()] or [locs2vect()].
+#' @param ras [terra::SpatRaster()] object
 #' @param layer Integer. Raster layer to use for comparing with point locations (default = 1).
 #'
 #' @return A logical vector.
 #' @export
 #' @examples
-#' points <- data.frame(lon = c(1, 2, 1, 2), lat = c(1, 1, 2, 3))
-#' points
-#' points.sf <- sf::st_as_sf(points, coords = c("lon", "lat"), crs = 4326)
+#' locs <- data.frame(lon = c(1, 2, 1, 2), lat = c(1, 1, 2, 3))
+#' locs.sf <- locs2sf(locs)
 #' ras <- rast(nrows = 2, ncols = 2, xmin = 0.5, xmax = 3.5, ymin = 0.5, ymax = 3.5,
-#' resolution = 1, vals = c(NA, 1, 1, NA, NA, 1, NA, 1, 1))
+#'  resolution = 1, vals = c(NA, 1, 1, NA, NA, 1, NA, 1, 1))
 #' plot(ras)
-#' plot(points.sf, add = T)
+#' plot(locs.sf, add = TRUE)
 #'
-#' point_in_cell(locs, ras)
+#' point_in_cell(locs.sf, ras)
 #'
-point_in_cell <- function(locs, ras, layer = 1) {
+#' # adding column to original point data
+#' locs.sf$inras <- point_in_cell(locs.sf, ras)
+#' locs.sf
+#'
+point_in_cell <- function(locs = NULL, ras = NULL, layer = 1) {
 
   if (!inherits(locs, "sf")) {
-    stop("locs must be a sf object. You may find 'locs2sf()' useful.")
+    if (!inherits(locs, "SpatVector")) {
+      stop("locs must be a sf or SpatVector object. You may find 'locs2sf()' or 'locs2vect()' useful.")
+    }
   }
+
+  stopifnot(is.numeric(layer))
+  stopifnot(layer >= 1 && layer <= terra::nlyr(ras))
 
   locs.terra <- terra::vect(locs)
 
@@ -36,6 +45,6 @@ point_in_cell <- function(locs, ras, layer = 1) {
   ## Get NA cells
   rasvals <- terra::extract(ras, locs.terra, layer = layer, raw = TRUE, ID = FALSE)
   missing <- is.na(rasvals)
-  missing
+  missing[, "value"]
 
 }
