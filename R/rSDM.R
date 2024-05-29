e4203125acc6ddecd2a6edce851765023db7ea84
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2024-05-29 19:49:24 +0200

    use pkg doc

--- R/rSDM.R
+++
@@ -1,8 +0,0 @@
-#' rSDM: functions for SDM and niche modelling in R
-#'
-#' rSDM: functions for SDM and niche modelling in R
-#'
-#' @keywords package
-#' @author F. Rodriguez-Sanchez
-#' @name rSDM
-NULL
